19395b55d2035fec4806fc7084b0d478072acb54
Max Wilde <prims@inbox.ru>
2020-06-24 22:12:12 +0700

    check posting

--- server/urls.py
+++ server/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from taxi import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('report/', views.get_report),
 ]
